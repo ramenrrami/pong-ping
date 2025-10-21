815c3d0dcdc02fbaa2f4e082b72ceb9d5da7995f
savio <savio@MacBook-Air-von-savio.local>
2025-10-21 15:41:35 +0200

    Update ARM64 build

--- src/main.cpp
+++ src/main.cpp
@@ -8,22 +8,20 @@
 Player player;
 
 int main() {
-    const int monitor = GetCurrentMonitor();
-    
-    const int screenWidth = 1920;
-    const int screenHeight = 1080;
+    const int BASE_WIDTH = 1920;
+    const int BASE_HEIGHT = 1080;
 
-    InitWindow(screenWidth, screenHeight, "Pong Ping v1.0");
+    const int monitor = GetCurrentMonitor();
+    const int screenWidth = GetMonitorWidth(monitor);
+    const int screenHeight = GetMonitorHeight(monitor);
+
+    (void) screenWidth;
+    (void) screenHeight;
+
+    SetConfigFlags(FLAG_FULLSCREEN_MODE);
+    InitWindow(BASE_WIDTH, BASE_HEIGHT, "Pong Ping v1.0");
 
     SetTargetFPS(60);
-
-    int monitorWidth = GetMonitorWidth(monitor);
-    int monitorHeight = GetMonitorHeight(monitor);
-
-    int posX = (monitorWidth - screenWidth) / 2;
-    int posY = (monitorHeight - screenHeight) / 2;
-
-    SetWindowPosition(posX, posY);
 
     /*Font Definer*/
     FontManager::LoadFont("ShinGoProBold", "resources/fonts/ATTFShinGoProBold.ttf", 64);
